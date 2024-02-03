02606574381d0be434f3dcc01bb2168d84c8501e
EathUrFoodBoi <123604375+EathUrFoodBoi@users.noreply.github.com>
2024-02-03 16:51:37 -0600

    Add files via upload

--- TeamCollection.py
+++ TeamCollection.py
@@ -1,45 +1,89 @@
-import tbapy, csv
-
+import tbapy, xlsxwriter
 tba = tbapy.TBA('GZ9erQoKBsgLfKlXDGClNWrdyVMq5Tpk3qab732jxDwaHAyAsL67WHl5Y8e5UtXY')
+currentYear = input("What is the current year?")
+allInfo = []
+workbook = xlsxwriter.Workbook('Team_Attrition_Data_2024.xlsx')
+worksheet = workbook.add_worksheet()
 
-currentYear = 2024
+try:
+    currentYear = int(currentYear)
+except:
+    print("Invalid number! Please restart program!")
+    exit()
 
+
+print("Getting the teams...")
 teams = tba.teams()
-rows = []
-state = 1
-for i in teams:
-    print(i.team_number)
-    years = tba.team_years(i.team_number)
+print("Teams returned!")
+def Earliest(teamNumber):
+    teamYears = tba.team_years(teamNumber)
+    earliestYear = teamYears[0]
+    return earliestYear
+def Latest(teamNumber):
+    teamYears = tba.team_years(teamNumber)
+    latestYear = teamYears[len(teamYears)-1]
+    return latestYear
+def stillActive(teamNumber):
+    latestYear = int(Latest(teamNumber))
+    if latestYear == currentYear:
+        return True
+    else:
+        return False
+def teamLeft(teamNumber):
+    teamYears = tba.team_years(teamNumber)
+    for i in range(len(teamYears) - 1):
+        if not (int(teamYears[i+1] == int(len(teamYears)-1))):
+            if not (int(teamYears[i+1]) == int(teamYears[i]) +1):
+                return (teamYears[i])
+    return False
+def teamReturned(teamNumber):
+    teamYears = tba.team_years(teamNumber)
+    if not teamLeft(teamNumber) == False:
+        yearLeft = teamLeft(teamNumber)
+        indexOfYear = int(teamYears.index(yearLeft))
+        yearReturned = teamYears[indexOfYear+1]
+        return yearReturned
+    else:
+        return False
+    
+def getCountry(whereInList):
+    country = teams[whereInList]["country"]
+    return country
 
-    try:
-        started = years[0]
-        last = years[-1]
-        competed = last - started + 1
-        if len(years) != (last - started + 1):
-            for j in range(len(years)):
-                if years[j+1] > years[j] + 1:
-                    last = years[j]
-                    returned = years[j+1]
-                    break
-        else:
-            returned = 0
-        years_range = [0] * (currentYear-1993+1)
-        for play_year in range(1993,currentYear,1):
-            if play_year in years:
-                years_range[play_year-1993] = 1
-            if started > play_year:
-                years_range[play_year-1993] = float("nan")
-        row = [i.team_number,i.city, i.state_prov, i.postal_code,i.lat,i.lng, started]
-        row.extend(years_range)
-    except:
-        print(".", end="")
-    rows.append(row)
-header = ["team number", "city", "state", "zip code", "latitude", "longitude", "rookie year", "1993", "1994", "1995", "1996", "1997", "1998", "1999","2000",
-          "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017",
-          "2018", "2019", "2020", "2021", "2022", "2023", "2024" ]
-with open("AllTeamData_years.csv", 'w', encoding="utf-8", newline='') as csvfile:
-    writer = csv.writer(csvfile, delimiter=",")
-    writer.writerow(header)
-    writer.writerows(rows)
+def getState(whereInList):
+    state_prov = teams[whereInList]["state_prov"]
+    return state_prov
 
+def getCity(whereInList):
+    city = teams[whereInList]["city"]
+    return city
+
+print("Writing info to worksheet...")
+worksheet.write(0, 0, "Team Number")
+worksheet.write(0, 1, "Year they Joined")
+worksheet.write(0, 2, "Most recent year")
+worksheet.write(0, 3, "Still active?")
+worksheet.write(0, 4, "Did they take a break? When did it start?")
+worksheet.write(0, 5, "When did they return?")
+worksheet.write(0, 6, "Country")
+worksheet.write(0, 7, "State/Province")
+worksheet.write(0, 8, "City")
+for i in range((len(teams) - 1)):
+    teamNumber = int(teams[i]["team_number"])
+    teamYears = tba.team_years(teamNumber)
+    if not(len(teamYears) == 0):
+        worksheet.write(i+1, 0, teamNumber)
+        worksheet.write(i+1, 1, str(Earliest(teamNumber)))
+        worksheet.write(i+1, 2, str(Latest(teamNumber)))
+        worksheet.write(i+1, 3, str(stillActive(teamNumber)))
+        worksheet.write(i+1, 4, str(teamLeft(teamNumber)))
+        worksheet.write(i+1, 5, str(teamReturned(teamNumber)))
+        worksheet.write(i+1, 6, str(getCountry(i)))
+        worksheet.write(i+1, 7, str(getState(i)))
+        worksheet.write(i+1, 8, str(getCity(i)))
+    if i%50 == 0:
+        print("The code is still running!")
+    if i==8000:
+        print("Almost done!")
+workbook.close()
 print("Done!")
